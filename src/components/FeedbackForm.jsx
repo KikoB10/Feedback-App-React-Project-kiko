afd8c2a6ac35e7a77d614fe5dc6af7d73cd71d05
KikoB10 <burten.kiko@gmail.com>
2023-01-12 13:44:25 -0600

    Added real time validation to when then Button in the Form can go active = once the message has atleast 10 characters

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,21 @@ import Button from './Shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be atleast 10 characters');
+      setBtnDisabled(true);
+    } else {
+      setMessage('');
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
 
@@ -21,8 +35,12 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
