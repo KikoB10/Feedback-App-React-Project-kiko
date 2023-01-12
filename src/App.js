4550d182c41ee0b7994f6c449702851951f04bb4
KikoB10 <burten.kiko@gmail.com>
2023-01-12 12:07:19 -0600

    practicing with conditional styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import React from "react";
-import Header from "./components/Header";
-import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
+import { useState } from 'react';
+import React from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,17 @@
-import Card from "./Shared/Card";
+import Card from './Shared/Card';
+import PropTypes from 'prop-types';
 
 function FeedbackItem({ item }) {
   return (
-    <Card reverse={true}>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
